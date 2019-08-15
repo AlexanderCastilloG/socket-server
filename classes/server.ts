3e052171587455f2b0a7aa30dc0ee862d5281e55
Alexander <gcastilloalex@gmail.com>
2019-08-15 17:38:06 -0500

    seccion 4

--- classes/server.ts
+++ classes/server.ts
@@ -1,19 +1,51 @@
 import express from "express";
 import { SERVER_PORT } from "../global/environment";
+import socketIO from 'socket.io';
+import http from 'http';
+
+import * as socket from '../sockets/sockets';
 
 export default class Server {
+
+    private static _instance: Server; // esta propiedad es del mismo tipo de la clase Server
 
     public app: express.Application;
     public port: number;
 
-    constructor() {
+    public io: socketIO.Server;
+    private httpServer: http.Server;
+
+    private constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+
+        this.escucharSockets();
     }
 
-    // tslint:disable-next-line:typedef
+    public static get instance() {
+        return this._instance || ( this._instance = new this());  // ese this es como Server()
+    }
+
+    private escucharSockets() {
+        console.log('Escuchando conexiones');
+
+        this.io.on('connection', cliente => {
+            console.log('Cliente conectado');
+
+            // Mensajes
+            socket.mensaje(cliente, this.io);
+
+            // Desconectar
+            socket.desconectar(cliente);
+
+        });
+    }
+
     start(callback: Function) {
-        this.app.listen(this.port, callback());
+        this.httpServer.listen(this.port, callback());
     }
 
 }
